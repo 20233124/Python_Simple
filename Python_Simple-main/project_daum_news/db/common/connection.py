455020690d2e7b019f42af645cb2578b74c6bd93
20233124 <144208691+20233124@users.noreply.github.com>
2023-11-10 14:34:47 +0900

    Add files via upload

--- Python_Simple-main/project_daum_news/db/common/connection.py
+++ Python_Simple-main/project_daum_news/db/common/connection.py
@@ -22,7 +22,49 @@
 #    - ID and PW
 #   2.SQL을 사용해서 작업(CRUD) 진행
 #    - SQL(구조질의어): RDB를 사용하기 위해서는 반드시 사용!
+#    - RDB(SQL)을 사용, NoSQL(SQL X)
 #    CREATE     -> INSERT
 #    READ       -> SELECT
 #    UPDATE     -> UPDATE
 #    DELETE     -> DELETE
+
+#  ** MongoDB 사용방법 2가지
+#  1.직접 설치(Local)
+#    - IP, PORT, ID, PW
+#    - 장점: 사용, 관리편함, 커스터마이징 가능
+#    - 단점: 설치과정 복잡, 설정 직접, 컴퓨터 자원 부족
+#  2. MongoDB에서 제공하는 Web Cloud 사용
+#    - URL, ID, PW
+#    - 장점: 사용편함, 설치 X, 설정 x, 컴퓨터 자원 x
+#    - 단점: 관리 x, 커스터마이징 x
+
+#  ** MongoDB구조
+#  설치: MongoDB(DBMS) : 시스템
+#           ㄴ DB(카카오톡) : 폴더
+#                ㄴ Collection(회원) : 표
+#                ㄴCollection(톡)
+#                ㄴCollection(친구)
+#                ㄴ ...
+#           ㄴ DB(카카오뱅크) : 폴더
+#                ㄴCollection(회원)
+#                ㄴCollection(계좌)
+#                ㄴCollection(대출)
+#                ㄴ ...
+#           ㄴ DB(카카오페이) : 폴더
+#                ㄴ ...
+
+# pymongo: Python - MongoDB 연결해서 사용
+# 왼쪽 아래  Terminal(Alt + F12)열어서 pip install pymongo
+from pymongo import MongoClient
+
+
+# MongoDB Connection
+def conn_mongodb():
+    # URL, ID, PW
+    DB_ID = "root"  # 상수 (전체 대문자로 변수명을 사용, 변하지않음)
+    DB_PW = "1234"  # 예시) 은행에서 금리(상수)
+    client = MongoClient(f"mongodb+srv://{DB_ID}:{DB_PW}@daumcluster.n0bbe0l.mongodb.net/")  # URL
+    db = client["daum"]
+    collection = db.get_collection("news")
+    return collection
+
